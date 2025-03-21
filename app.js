e528b8a954e5e6e2f7f209770089de65aba48f2b
Syed Gulzar Jilani <syedgulzarmohammad123@gmail.com>
2025-03-21 14:01:57 -0700

    title frame is in process

---
+++ app.js
@@ -0,0 +1,14 @@
+let index = 0;
+        const visibleItems = 4;
+        const itemWidth = document.querySelector('.slider-item').offsetWidth + 20; // Item width + gap
+        const slider = document.getElementById('slider');
+        const totalItems = slider.children.length;
+
+        function moveSlide(direction) {
+            if (direction === 1 && index < totalItems - visibleItems) {
+                index++;
+            } else if (direction === -1 && index > 0) {
+                index--;
+            }
+            slider.style.transform = `translateX(-${index * itemWidth}px )`;
+        }
